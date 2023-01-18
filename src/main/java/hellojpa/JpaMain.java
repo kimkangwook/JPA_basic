4e7837c8f3dd0f568d3b1c6afd56dce1f5b26558
kangwook <rlarkddnr1686@naver.com>
2023-01-18 13:10:17 +0900

    JsonType 설정
    # DB에 JsonType으로 데이터 넣기
      - Json 문자열 넣기 : 하나의 Json 객체로 들어가짐
      - Json 컬럼으로 컬렉션 넣기 : Json 배열 형태로 들어가짐

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,16 @@
 package hellojpa;
 
+import jdk.swing.interop.SwingInterOpUtils;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,21 +23,50 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember.id() = " + findMember.getId());
+            ChildParam child = new ChildParam();
+            child.setAge(15);
+            child.setName("김강욱");
+            ChildParam child2 = new ChildParam();
+            child2.setAge(18);
+            child2.setName("김강욱2");
+            em.persist(child);
+            em.persist(child2);
 
-//            em.remove(findMember);
-//            findMember.setName("HelloJPA");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5).setMaxResults(8).getResultList();//JPQL 기본 사용법
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
+            System.out.println("=================1=============");
+            ArrayList<ChildParam> ar = new ArrayList<>();
+            ar.add(child);
+            ar.add(child2);
+
+            System.out.println("==================2============");
+
+            ParentEntity parent = new ParentEntity();
+            parent.setChilds(ar);
+            parent.setMbr("{\"ITEM\":\"MUSIC\",\"ITEM\":\"BOOK\"}");
+//            parent.setMbr("{\"ITEM\":\"BOOK\"}");
+//            parent.setMbr("{\"ITEM\":\"MOVIE\"}");
+            System.out.println("========3==========");
+            em.persist(parent);
+            System.out.println("==============4================");
+
+            em.flush();
+            em.clear();
+            System.out.println("===============5================");
+            ParentEntity parent1 = em.find(ParentEntity.class, parent.getId());
+            System.out.println("result : "+ (parent==parent1));
+            System.out.println("parent1.getChilds().getClass() = " + parent1.getChilds().getClass());
+            List<ChildParam> childs = parent1.getChilds();
+            for (ChildParam childParam : childs) {
+                System.out.println("childParam.NAME = " + childParam.getName());
+                System.out.println("childParam.AGE = " + childParam.getAge());
             }
-            tx.commit();
+            String mbr = parent1.getMbr();
+            System.out.println("mbr = " + mbr);
 
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,19 +1,28 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
+//@Table(uniqueConstraints = )
 //@Table(name="USER")
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+//@SequenceGenerator(
+//        name = "MEMBER_SEQ_GENERATOR" ,
+//        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1, allocationSize = 50)
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
+    @Column(name="MEMBER_ID")
     private Long id;
 
-//    @Column(name="userName")
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
 
     public Long getId() {
         return id;
@@ -23,11 +32,69 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    //기간 Period
+    @Embedded
+    private Period workPeriod;
+
+    //주소
+    @Embedded
+    private Address homeAddress;
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+            @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name="FOOD_NAME") // 예외적으로
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    //    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//            @JoinColumn(name="MEMBER_ID")
+//    )
+//    private List<Address> addressHistory = new ArrayList<>();
+    @OneToMany(cascade = ALL,orphanRemoval = true)
+    @JoinColumn(name="MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+
 }
